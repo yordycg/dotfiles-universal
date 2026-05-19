39af2137a21882f7fd338f3d89765025248c85c0
yordycg <yordy.carmona8@gmail.com>
2026-05-19 11:50:22 -0400

    cleanup: remove redundant installation scripts replaced by mise

--- scripts/run_once_install-starship.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Instalar starship si no esta disponible en los repositorios
-if ! command -v starship &>/dev/null; then
-	echo "== Instalando starship..."
-	curl -sS https://starship.rs/install.sh | sh -s -- --yes
-fi
