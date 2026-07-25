14f1b5fe066cc5a03b16f665ee6763e05879b7f5
yordycg <yordy.carmona8@gmail.com>
2026-07-24 22:59:24 -0400

    feat(themes): add Everforest theme palette and configuration

---
+++ dot_config/themes/everforest/hypr/colors.lua
@@ -0,0 +1,27 @@
+local M = {
+	bg0 = "rgb(2b3339)",
+	bg1 = "rgb(323c41)",
+	bg2 = "rgb(3a454a)",
+	bg3 = "rgb(445055)",
+	bg4 = "rgb(4c565a)",
+
+	fg = "rgb(d3c6aa)",
+	red = "rgb(e67e80)",
+	orange = "rgb(e69875)",
+	yellow = "rgb(dbbc7f)",
+	green = "rgb(a7c080)",
+	aqua = "rgb(83c092)",
+	blue = "rgb(7fbbb3)",
+	purple = "rgb(d699b6)",
+
+	grey0 = "rgb(9da9a0)",
+	grey1 = "rgb(859289)",
+	grey2 = "rgb(7a8478)",
+
+	shadow_color = 0xee1a2124,
+}
+
+M.active_border = M.green
+M.inactive_border = M.bg3
+
+return M
